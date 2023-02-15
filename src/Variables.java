9327abca90f34cb54c64fd564299407a904c16b6
borja <borja@example.com>
2023-02-15 18:44:13 +0100

    video 7 operadores y constantes

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        byte edad=35;//Declaración e inicialización de variable en una única línea
+
+        /* la siguente instrucción imprime en la consola (System.out) el valor de la
+        variable edad */
+        System.out.println(edad);
+        edad=75;
+
+        System.out.println(edad );
+
+    }
+}
