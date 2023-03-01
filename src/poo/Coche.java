0e651510ce113dd5ffa89b111d95325fa528f838
borja <borja@example.com>
2023-03-01 13:46:28 +0100

    video32 OK

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -5,20 +5,13 @@ import java.awt.*;
 public class Coche {
 
     private int ruedas;
-
     private int largo;
-
     private int ancho;
-
     private int motor;
-
     private int pesoPlataforma;
-
     private String color;
-
     private int pesoTotal;
-
-    private boolean asientosCuero, climatizador;
+    private boolean asientosCuero, climatizador;//estas variable van a afectar al precio y al peso del coche
 
     public Coche(){//el método constructor se llama igual que la clase y se encarga
                     //de dar un estado inicial al objeto que se crea
@@ -27,46 +20,80 @@ public class Coche {
         ancho=300;
         pesoPlataforma=500;
         motor=1600;
-
     }
+
     public String dimeDatosGenerales(){//GETTER
 
         return "La plataforma del vehículo tiene " + ruedas + " ruedas." +
                 " Mide " + largo/1000 + " metros con un ancho de " + ancho +
                 " cm y un peso de plataforma de " + pesoPlataforma + "kg";
-
     }
 
     public void estableceColor(String colorCoche){//SETTER
-
         color=colorCoche;
-
     }
 
     public String dimeColor(){
-
         return "El color de tu coche es " + color;
-
     }
 
     public void configuraAsientos(String asientosCuero){//SETTER
-
-        if(asientosCuero=="si"){
-
+        if(asientosCuero.equalsIgnoreCase("si")){
             this.asientosCuero=true;
-
         }else{
             this.asientosCuero=false;
         }
-
     }
 
-    public String dimeAsientos() {
-
+    public String dimeAsientos() {//GETTER
         if(asientosCuero==true){
             return "El coche tiene asientos de cuero";
         }else {
-            return "El coche no tiene asientos de serie";
+            return "El coche no tiene asientos de cuero";
         }
     }
+
+    public void configuraClimatizador(String climatizador){//SETTER
+        if (climatizador.equalsIgnoreCase("si")){
+            this.climatizador=true;
+        }else{
+            this.climatizador=false;
+        }
+    }
+
+    public String dimeClimatizador(){//GETTER
+        if (climatizador==true){
+            return "El coche incorpora climatizador";
+        }else{
+            return "El coche lleva aire acondicionado";
+        }
+    }
+
+    public String dimePesoCoche(){//setter y getter al mismo tiempo
+
+        int pesoCarroceria=500;
+
+        pesoTotal=pesoPlataforma+pesoCarroceria;
+        if (asientosCuero==true){
+            pesoTotal+=50;
+        }
+        if (climatizador==true){
+            pesoTotal+=20;
+        }
+        return "El peso total del coche es " + pesoTotal;
+    }
+
+    public int precioCoche(){//getter
+
+        int precioFinal=10000;
+
+        if(asientosCuero==true){
+            precioFinal+=2000;
+        }
+        if(climatizador==true){
+            precioFinal+=1500;
+        }
+        return precioFinal;
+    }
+
 }
--- src/poo/UsoCoche.java
+++ src/poo/UsoCoche.java
@@ -1,17 +1,24 @@
 package poo;
 
+import javax.swing.*;
+
 public class UsoCoche {
     public static void main(String[] args) {
 
        Coche miCoche=new Coche();
 
-       miCoche.estableceColor("marrón");
-       miCoche.configuraAsientos("si");
+       miCoche.estableceColor(JOptionPane.showInputDialog("Introduzca color"));
+       miCoche.configuraAsientos(JOptionPane.showInputDialog("¿Tiene asientos de cuero?"));
+       miCoche.configuraClimatizador(JOptionPane.showInputDialog("¿Tiene climatizador?"));
 
 
        System.out.println(miCoche.dimeDatosGenerales());
        System.out.println(miCoche.dimeColor());
        System.out.println(miCoche.dimeAsientos());
+       System.out.println(miCoche.dimeClimatizador());
+       System.out.println(miCoche.dimePesoCoche());
+       System.out.println("El precio final del coche es " + miCoche.precioCoche());
+
 
     }
 }
